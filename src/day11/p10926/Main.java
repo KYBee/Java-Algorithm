3fd50d12e297bc68737f66b09d9759a77dd82fcc
KYBee <kyb3634@naver.com>
2022-01-27 17:47:45 +0900

    Day 11

---
+++ src/day11/p10926/Main.java
@@ -0,0 +1,18 @@
+package day11.p10926;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(st.nextToken()).append("??!");
+        System.out.println(sb.toString());
+    }
+}
